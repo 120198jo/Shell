ef147a2a543ff9bfaa9e17a85ebe1e6b9d9970a4
joseph kouadio <josephkouadio82@gmail.com>
2022-12-16 16:08:24 -0800

    test

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.c>
+
+int main() {
+	int bock;
+	double number1;
+
+	bock = 320;
+	number1 = 450.5;
+
+	printf("price of bock : %d\n", bock);
+	printf("price of number1 : %lf\n", number1);
+
+	return 0;
+}
